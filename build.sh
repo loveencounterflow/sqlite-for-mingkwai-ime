33aa8879f0adbfe4f4d57902a2bae4e2effdff4a
loveencounterflow <flow@orion.nebula>
2019-02-26 12:55:19 +0100

    set version number

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -euo pipefail
 
-targetname=sqlite3-27-2a
+targetname=sqlite3-27-902
 echo "building $targetname"
 # thx to https://www.sqlite.org/howtocompile.html
 gcc shell.c sqlite3.c -lpthread -ldl -o "$targetname"
