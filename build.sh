6454d14e704d6903eee85c947df47c9f13e9d512
loveencounterflow <flow@orion.nebula>
2019-02-26 12:48:14 +0100

    add buildscript

--- build.sh
+++ build.sh
@@ -3,6 +3,7 @@ set -euo pipefail
 
 targetname=sqlite3-27-2a
 echo "building $targetname"
+# thx to https://www.sqlite.org/howtocompile.html
 gcc shell.c sqlite3.c -lpthread -ldl -o "$targetname"
 
 
