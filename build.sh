e346737322554a2bd7d5a68ae9398c9cc1d82f49
loveencounterflow <flow@orion.nebula>
2019-02-26 17:48:18 +0100

    correct buildscript to include FTS5, math librariws (`gcc -lm` switch)

--- build.sh
+++ build.sh
@@ -4,7 +4,16 @@ set -euo pipefail
 targetname=sqlite3-27-902
 echo "building $targetname"
 # thx to https://www.sqlite.org/howtocompile.html
-gcc shell.c sqlite3.c -lpthread -ldl -o "$targetname"
+# gcc shell.c sqlite3.c -lpthread -ldl -o "$targetname"
+
+# thx to https://www.sqlite.org/howtocompile.html
+# `-lm` switch thx to https://www.mail-archive.com/sqlite-users@mailinglists.sqlite.org/msg93715.html
+gcc -Os -I. -DSQLITE_THREADSAFE=0 -DSQLITE_ENABLE_FTS4 \
+   -DSQLITE_ENABLE_FTS5 -DSQLITE_ENABLE_JSON1 \
+   -DSQLITE_ENABLE_RTREE -DSQLITE_ENABLE_EXPLAIN_COMMENTS \
+   -DHAVE_USLEEP -DHAVE_READLINE \
+   shell.c sqlite3.c -ldl -lm -lreadline -lncurses -o "$targetname"
 
 
-
+# `-lm` switch thx to https://www.mail-archive.com/sqlite-users@mailinglists.sqlite.org/msg93715.html
+# gcc -Os shell.c sqlite3.c -DSQLITE_ENABLE_FTS5 -lpthread -lm -ldl -o "$targetname"
