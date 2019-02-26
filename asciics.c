aaf2fd95f361004834b7e43d60e16a502c1670d1
loveencounterflow <flow@orion.nebula>
2019-02-26 21:36:47 +0100

    implement actual case-sensitivity / non-folding property

--- asciics.c
+++ asciics.c
@@ -106,7 +106,7 @@ static void asciicsFold(char *aOut, const char *aIn, int nByte){
   int i;
   for(i=0; i<nByte; i++){
     char c = aIn[i];
-    if( c>='A' && c<='Z' ) c += 32;
+    // if( c>='A' && c<='Z' ) c += 32;
     aOut[i] = c;
   }
 }
