445cff9bd8d2da95bd5762fed4ebb1f1e4b43631
loveencounterflow <flow@orion.nebula>
2019-03-06 17:23:20 +0100

    cleanup

--- build.sh
+++ build.sh
@@ -8,11 +8,19 @@ echo "building $targetname"
 
 # thx to https://www.sqlite.org/howtocompile.html
 # `-lm` switch thx to https://www.mail-archive.com/sqlite-users@mailinglists.sqlite.org/msg93715.html
-gcc -Os -I. -DSQLITE_THREADSAFE=0 -DSQLITE_ENABLE_FTS4 \
-   -DSQLITE_ENABLE_FTS5 -DSQLITE_ENABLE_JSON1 \
-   -DSQLITE_ENABLE_RTREE -DSQLITE_ENABLE_EXPLAIN_COMMENTS \
-   -DHAVE_USLEEP -DHAVE_READLINE \
-   shell.c sqlite3.c -ldl -lm -lreadline -lncurses -o "$targetname"
+gcc \
+  -Os                                     \
+  -I.                                     \
+  -DSQLITE_THREADSAFE=0                   \
+  -DSQLITE_ENABLE_FTS4                    \
+  -DSQLITE_ENABLE_FTS5                    \
+  -DSQLITE_DEFAULT_FOREIGN_KEYS=1         \
+  -DSQLITE_ENABLE_JSON1                   \
+  -DSQLITE_ENABLE_RTREE                   \
+  -DSQLITE_ENABLE_EXPLAIN_COMMENTS        \
+  -DHAVE_USLEEP                           \
+  -DHAVE_READLINE                         \
+  shell.c sqlite3.c -ldl -lm -lreadline -lncurses -o "$targetname"
 
 
 # `-lm` switch thx to https://www.mail-archive.com/sqlite-users@mailinglists.sqlite.org/msg93715.html
