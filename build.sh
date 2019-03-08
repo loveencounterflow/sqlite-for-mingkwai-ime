d943c11f06e0d7ce5a60c64cfa6f21fdf5a9d206
loveencounterflow <flow@orion.nebula>
2019-03-08 22:27:19 +0100

    put compile options into sqlite3.c

--- build.sh
+++ build.sh
@@ -8,20 +8,25 @@ echo "building $targetname"
 
 # thx to https://www.sqlite.org/howtocompile.html
 # `-lm` switch thx to https://www.mail-archive.com/sqlite-users@mailinglists.sqlite.org/msg93715.html
+
+# !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+# !!! observe these options are repeated in sqlite3.c, so they will get picked up by                 		!!!
+# !!! better-sqlite3, too                                                                               !!!
+# !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 gcc \
   -Os                                     \
   -I.                                     \
-  -DSQLITE_THREADSAFE=0                   \
-  -DSQLITE_ENABLE_FTS4                    \
-  -DSQLITE_ENABLE_FTS5                    \
-  -DSQLITE_DEFAULT_FOREIGN_KEYS=1         \
-  -DSQLITE_ENABLE_JSON1                   \
-  -DSQLITE_ENABLE_RTREE                   \
-  -DSQLITE_ENABLE_EXPLAIN_COMMENTS        \
   -DHAVE_USLEEP                           \
   -DHAVE_READLINE                         \
   shell.c sqlite3.c -ldl -lm -lreadline -lncurses -o "$targetname"
 
+  # -DSQLITE_THREADSAFE=0                   \
+  # -DSQLITE_ENABLE_FTS4                    \
+  # -DSQLITE_ENABLE_FTS5                    \
+  # -DSQLITE_DEFAULT_FOREIGN_KEYS=1         \
+  # -DSQLITE_ENABLE_JSON1                   \
+  # -DSQLITE_ENABLE_RTREE                   \
+  # -DSQLITE_ENABLE_EXPLAIN_COMMENTS        \
 
 # `-lm` switch thx to https://www.mail-archive.com/sqlite-users@mailinglists.sqlite.org/msg93715.html
 # gcc -Os shell.c sqlite3.c -DSQLITE_ENABLE_FTS5 -lpthread -lm -ldl -o "$targetname"
